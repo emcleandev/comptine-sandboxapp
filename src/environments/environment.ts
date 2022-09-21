b66eaea58f1e9c4ba85e0056b8bfda41ea2ec65d
Emmanuel Mclean <emclean.dev@gmail.com>
2022-09-21 13:30:43 +0100

    +

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDODLVMuwrvGobMXkI89DktOLgwf0mCM24",
+    authDomain: "joaq-lab.firebaseapp.com",
+    databaseURL: "https://joaq-lab.firebaseio.com",
+    projectId: "joaq-lab",
+    storageBucket: "joaq-lab.appspot.com",
+    messagingSenderId: "794748950011",
+    appId: "1:794748950011:web:815fe385e7317c11"
+  },
+  userApi: 'http://localhost:5000/joaq-lab/us-central1/api'
 };
 
 /*
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
